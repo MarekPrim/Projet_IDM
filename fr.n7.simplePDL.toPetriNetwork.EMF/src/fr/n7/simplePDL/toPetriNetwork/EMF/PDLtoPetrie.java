34013a760bec161a85f852a0eae46c0c3c7b068f
Marek <59203712+MarekPrim@users.noreply.github.com>
2023-03-16 15:39:36 +0100

    fix emf transfo + pdl & petrie examples (success and failing most OCL props)

--- fr.n7.simplePDL.toPetriNetwork.EMF/src/fr/n7/simplePDL/toPetriNetwork/EMF/PDLtoPetrie.java
+++ fr.n7.simplePDL.toPetriNetwork.EMF/src/fr/n7/simplePDL/toPetriNetwork/EMF/PDLtoPetrie.java
@@ -1,7 +1,10 @@
 package fr.n7.simplePDL.toPetriNetwork.EMF;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
@@ -9,7 +12,9 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 
+import petrie.Arc;
 import petrie.ArcRead;
 import petrie.ArcSimple;
 import petrie.PetriNet;
@@ -25,12 +30,20 @@ import simplepdl.SimplepdlPackage;
 import simplepdl.WorkDefinition;
 import simplepdl.WorkSequence;
 import simplepdl.WorkSequenceType;
-
+/*
+ * This class is used to generate a PetriNet from a SimplePDL model.
+ * @authors: Carl Andriamanjato; Kilyan Le Gallic
+ * @version: 1.1
+ * @date: 2023-03-16
+ * For information concerning how to save an EMF Model, check https://stackoverflow.com/questions/25864816/saving-an-emf-model
+ *
+ * Code inspired from https://www.eclipse.org/forums/index.php/t/1078143/
+ */
 public class PDLtoPetrie {
 
 	public static Process loadProcess(String modelPath) {
 
-		// Chargement du package SimplePDL afin de l'enregistrer dans le registre d'Eclipse.
+		
 		SimplepdlPackage packageInstance = SimplepdlPackage.eINSTANCE;
 		
 		// Enregistrer l'extension ".xmi" comme devant être ouverte à
@@ -40,9 +53,10 @@ public class PDLtoPetrie {
 		m.put("xmi", new XMIResourceFactoryImpl());
 		
 		// Créer un objet resourceSetImpl qui contiendra une ressource EMF (notre modèle)
+		
 		ResourceSet resSet = new ResourceSetImpl();
 
-		// Charger la ressource (notre modèle)
+		// Charger la ressource
 		URI modelURI = URI.createURI(modelPath);
 		Resource resource = resSet.getResource(modelURI, true);
 		
@@ -69,51 +83,60 @@ public class PDLtoPetrie {
  		PetriNet petrie = myFactory.createPetriNet();
  		petrie.setName(process.getName());
  		
- 		resource.getContents().add(petrie);
- 		
- 		process.getProcessElement().stream().forEach(pe -> {
- 			if (pe instanceof WorkDefinition) {		// WorkDefinition
-				WorkDefinition wd = (WorkDefinition) pe;
- 				
+ 		XMIResourceImpl r_esource = new XMIResourceImpl();
+ 		 File source = new File("transformation"+modelPath+".xmi");
+ 		 try {
+			r_esource.load( new FileInputStream(source), new HashMap<Object,Object>());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
- 				// WorkDefinition Places
- 				Place ready = myFactory.createPlace();
- 	 			ready.setName(wd.getName() + "_ready");
- 	 			ready.setMarking(1);
- 	 			ready.setPetrinet(petrie);
- 	 			petrie.getPetriElements().add(ready);
- 	 			
- 	 			Place running = myFactory.createPlace();
- 	 			running.setName(wd.getName() + "_running");
- 	 			running.setMarking(0);
- 	 			running.setPetrinet(petrie);
- 	 			petrie.getPetriElements().add(running);
- 	 			
- 	 			Place started = myFactory.createPlace();
- 	 			started.setName(wd.getName() + "_started");
- 	 			started.setMarking(0);
- 	 			started.setPetrinet(petrie);
- 	 			petrie.getPetriElements().add(started);
- 	 			
- 	 			Place finished = myFactory.createPlace();
- 	 			finished.setName(wd.getName() + "_finished");
- 	 			finished.setMarking(0);
- 	 			finished.setPetrinet(petrie);
- 	 			petrie.getPetriElements().add(finished);
- 	 			
+ 		
+ 		
+
+
+		process.getProcessElement().stream().forEach(pe ->
+		{
+			if (pe instanceof WorkDefinition){
+				WorkDefinition wd = (WorkDefinition) pe;
+
+				Place ready = myFactory.createPlace();
+	 			ready.setName(wd.getName() + "_ready");
+	 			ready.setMarking(1);
+	 			ready.setPetrinet(petrie);
+	 			petrie.getPetriElements().add(ready);
+	 			
+	 			Place running = myFactory.createPlace();
+	 			running.setName(wd.getName() + "_running");
+	 			running.setMarking(0);
+	 			running.setPetrinet(petrie);
+	 			petrie.getPetriElements().add(running);
+	 			
+	 			Place started = myFactory.createPlace();
+	 			started.setName(wd.getName() + "_started");
+	 			started.setMarking(0);
+	 			started.setPetrinet(petrie);
+	 			petrie.getPetriElements().add(started);
+	 			
+	 			Place finished = myFactory.createPlace();
+	 			finished.setName(wd.getName() + "_finished");
+	 			finished.setMarking(0);
+	 			finished.setPetrinet(petrie);
+	 			petrie.getPetriElements().add(finished);
+	 			
 				
- 	 			// WorkDefinition Transitions
- 	 			Transition start = myFactory.createTransition();
- 	 			start.setName(wd.getName() + "_start");
- 	 			start.setPetrinet(petrie);
- 	 			petrie.getPetriElements().add(start);
- 	 			
- 	 			Transition finish = myFactory.createTransition();
- 	 			finish.setName(wd.getName() + "_finish");
- 	 			finish.setPetrinet(petrie);
- 	 			petrie.getPetriElements().add(finish);
- 	 			
-				
+	 			// WorkDefinition Transitions
+	 			Transition start = myFactory.createTransition();
+	 			start.setName(wd.getName() + "_start");
+	 			start.setPetrinet(petrie);
+	 			petrie.getPetriElements().add(start);
+	 			
+	 			Transition finish = myFactory.createTransition();
+	 			finish.setName(wd.getName() + "_finish");
+	 			finish.setPetrinet(petrie);
+	 			petrie.getPetriElements().add(finish);
+	 			
  	 			// WorkDefinition Arcs
  	 			ArcSimple ready2start = myFactory.createArcSimple();
  	 			ready2start.setWeight(1);
@@ -149,25 +172,13 @@ public class PDLtoPetrie {
  	 			finish2finished.setTarget(finished);
  	 			finish2finished.setPetrinet(petrie);
  	 			petrie.getPetriElements().add(finish2finished);
- 			} else if (pe instanceof WorkSequence) {		// WorkSequence
- 				WorkSequence ws = (WorkSequence) pe;
- 				
- 				ArcRead aws = myFactory.createArcRead();
- 	 			
- 	 			aws.setSource((Node) petrie.getPetriElements().stream().filter(p -> p instanceof Node).filter(n -> ((Node) n).getName()
- 	 					.equals(ws.getPredecessor().getName() + (ws.getLinkType() == WorkSequenceType.START_TO_START
- 	 					|| ws.getLinkType() == WorkSequenceType.START_TO_FINISH ? "_started" : "_finished")) ).toArray()[0]);
-// 	 			System.out.println(petrie.getPetriElements().stream().filter(p -> p instanceof Node).filter(n -> ((Node) n).getName()
-// 	 					.equals(ws.getSuccessor().getName() + (ws.getLinkType() == WorkSequenceType.START_TO_START
-// 	 					|| ws.getLinkType() == WorkSequenceType.FINISH_TO_START ? "_start" : "_finish"))).count());
- 	 			aws.setTarget((Node) petrie.getPetriElements().stream().filter(p -> p instanceof Node).filter(n -> ((Node) n).getName()
- 	 					.equals(ws.getSuccessor().getName() + (ws.getLinkType() == WorkSequenceType.START_TO_START
- 	 					|| ws.getLinkType() == WorkSequenceType.FINISH_TO_START ? "_start" : "_finish")) ).toArray()[0]);
- 	 			
- 	 			aws.setPetrinet(petrie);
- 	 			petrie.getPetriElements().add(aws);
- 			} else if (pe instanceof simplepdl.Ressource) {		// Resources
- 				simplepdl.Ressource rs = (Ressource) pe;
+			}
+		});
+		
+		process.getProcessElement().stream().forEach(pe ->
+		{
+			if (pe instanceof Ressource) {		// Resources
+ 				Ressource rs = (Ressource) pe;
  				
  				Place prs = myFactory.createPlace();
  				prs.setName(rs.getName() + "_resource");
@@ -194,12 +205,55 @@ public class PDLtoPetrie {
  	 				petrie.getPetriElements().add(anRelease);
  				}
  			}
- 		});
- 		
- 		// Sauver la ressource
-	    try {
-	    	resource.save(Collections.EMPTY_MAP);
+		});
+		
+		process.getProcessElement().stream().forEach(pe -> {
+			if (pe instanceof WorkSequence) {		// WorkSequence
+ 				WorkSequence ws = (WorkSequence) pe;
+ 				
+ 				ArcRead aws = myFactory.createArcRead();
+ 	 			
+ 	 			aws.setSource((Node) petrie.getPetriElements().stream().filter(p -> p instanceof Node).filter(n -> ((Node) n).getName()
+ 	 					.equals(ws.getPredecessor().getName() + (ws.getLinkType() == WorkSequenceType.START_TO_START
+ 	 					|| ws.getLinkType() == WorkSequenceType.START_TO_FINISH ? "_started" : "_finished")) ).toArray()[0]);
+ 	 			
+ 	 			aws.setTarget((Node) petrie.getPetriElements().stream().filter(p -> p instanceof Node).filter(n -> ((Node) n).getName()
+ 	 					.equals(ws.getSuccessor().getName() + (ws.getLinkType() == WorkSequenceType.START_TO_START
+ 	 					|| ws.getLinkType() == WorkSequenceType.FINISH_TO_START ? "_start" : "_finish")) ).toArray()[0]);
+ 	 			
+ 	 			aws.setPetrinet(petrie);
+ 	 			petrie.getPetriElements().add(aws);
+			}
+		});
+
+		petrie.getPetriElements().stream().forEach( pe -> {
+			if (pe instanceof ArcSimple) {
+				ArcSimple ac = (ArcSimple) pe;
+				System.out.println(ac.getSource().getName()+" -"+ac.getWeight()+"> "+ac.getTarget().getName());
+			}
+			if (pe instanceof ArcRead) {
+				ArcRead ac = (ArcRead) pe;
+				System.out.println(ac.getSource().getName()+" -"+"read"+"> "+ac.getTarget().getName());
+			}
+		});
+//		resource.getContents().add(petrie);
+// 		// Sauver la ressource
+//	    try {
+//	    	resource.save(resource.getContents());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("key", new XMIResourceFactoryImpl());
+		ResourceSet res_Set = new ResourceSetImpl();
+		Resource re_source = resSet.createResource(URI.createFileURI("transformation"+modelPath+".xmi"));
+		re_source.getContents().add(petrie);
+		try {
+			re_source.save(Collections.EMPTY_MAP);
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
  		
